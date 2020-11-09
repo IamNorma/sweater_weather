af490fde7eb8d521e177702a9716c99c7199465e
Norma Lopez <norma_l304@yahoo.com>
2020-11-09 10:17:43 -0700

    Add spec for trail object

---
+++ spec/poros/trail_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe Trail do
+  it "exists" do
+    trail = {
+      "location": 'denver,co',
+      "forecast": {
+        "summary": 'Cloudy with a chance of meatballs',
+        "temperature": "83"
+      },
+      "trails": [
+        {
+          "name": "Boulder Skyline Traverse",
+          "summary": "The classic long mountain route in Boulder.",
+          "difficulty": "black",
+          "location": "Superior, Colorado",
+          "distance_to_trail": '23.008'
+        }
+      ]
+    }
+
+    trail = Trail.new(trail_data)
+
+    expect(trail).to be_a(Trail)
+  end
+end
