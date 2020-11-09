bcc1957d756b65fba36d9b67911adc14c477cebc
Norma Lopez <norma_l304@yahoo.com>
2020-11-08 20:35:15 -0700

    Change predicted_temp to be temperature

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    weather = WeatherFacade.city_weather(params[:location])
+    weather = WeatherFacade.fetch_city_weather(params[:location])
     render json: ForecastSerializer(weather)
   end
 end
