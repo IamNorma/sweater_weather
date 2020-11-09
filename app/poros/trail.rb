fcfd65323fd8876d46de032d8fa0f10833679f25
Norma Lopez <norma_l304@yahoo.com>
2020-11-09 11:09:22 -0700

    Add #temperature_summary()

--- app/poros/trail.rb
+++ app/poros/trail.rb
@@ -4,4 +4,11 @@ class Trail
     @forecast = temperature_summary(forecast)
     @trails = nearby_trails(trail_params[:trails])
   end
+
+  def temperature_summary(forecast)
+    {
+    "summary": forecast[:weather][0][:description],
+    "temperature": forecast[:temp].to_s
+    }
+  end
 end
