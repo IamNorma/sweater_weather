7457c069aa07c9270b4f709921443a8561f4c5e5
Norma Lopez <norma_l304@yahoo.com>
2020-11-09 00:58:32 -0700

    Change #next_eight_hours to #abridged_hours

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -1,9 +1,15 @@
 class Weather
   attr_reader :current, :hourly, :daily
-  
+
   def initialize(weather_params)
     @current_weather = CurrentWeather.new(weather_params[:current])
-    @hourly_weather = next_eight_hours(weather_params[:hourly])
+    @hourly_weather = abridged_hours(weather_params[:hourly])
     @daily_weather = next_five_days(weather_params[:daily])
+  end
+
+  def abridged_hours(hourly_weather)
+    hourly_weather.first(8).map do |hour_data|
+      HourlyWeather.new(hour_data)
+    end
   end
 end
