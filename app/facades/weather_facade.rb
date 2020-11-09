87fd597ce555e7821194d6a109cfcfbc2c88ddf7
Norma Lopez <norma_l304@yahoo.com>
2020-11-08 20:57:31 -0700

    Change data to weather_data for more clarity

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,7 +1,7 @@
 class WeatherFacade
   def self.fetch_city_weather(location)
     coordinates = MapService.geocode(location)
-    data = WeatherService.weather_forecast(coordinates)
-    Weather.new(data)
+    weather_data = WeatherService.weather_forecast(coordinates)
+    Weather.new(weather_data)
   end
 end
