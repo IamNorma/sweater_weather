1be8657a79c8448560c2598d7bf8e7fe98b08c01
Norma Lopez <norma_l304@yahoo.com>
2020-11-08 21:55:30 -0700

    Refactor Service by pulling out connection into its own private method

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,13 +1,16 @@
 class MapService
   def self.geocode(location)
-    conn = Faraday.new(url: 'http://www.mapquestapi.com') do |req|
-      req.params['key'] = ENV['MAP_API_KEY']
-    end
-
     response = conn.get('/geocoding/v1/address') do |req|
       req.params['location'] = location
     end
-
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new(url: 'http://www.mapquestapi.com') do |req|
+      req.params['key'] = ENV['MAP_API_KEY']
+    end
   end
 end
