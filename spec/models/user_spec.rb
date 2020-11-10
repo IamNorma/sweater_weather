2a5364e3bb12cc5f7cbfabb701f7cc6b0aa50483
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 14:00:35 -0700

    Add test for model method #set_api_key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:password_confirmation) }
   end
+
+  describe 'instance methods' do
+    it '.set_api_key' do
+      user = User.create!(email: 'example@email.com', password: '123pass', password_confirmation: '123pass')
+
+      expect(user.api_key).to be_a(String)
+      expect(user.api_key.size).to eq(28)
+    end
+  end
 end
