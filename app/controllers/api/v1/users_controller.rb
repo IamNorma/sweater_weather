8c7d1d9acf6c2a04444837fd9a205af002b025d8
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 12:47:50 -0700

    Update create action to create user with happy and sad path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def create
-
+    user = User.new(user_params)
+    if user.save
+      render json: UsersSerializer(user), status: 201
+    else
+      render json: { error: user.errors.full_messages.to_sentence }, status: 401
+    end
   end
 end
