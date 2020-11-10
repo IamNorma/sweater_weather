7a79d52b65a3d966be3f8f01e12c49aea4323bb8
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 13:46:33 -0700

    Update user model to generate api key before a user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
+require 'securerandom'
+
 class User < ApplicationRecord
+  before_create :set_api_key
+
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
 
-  has_secure_password 
+  has_secure_password
+
+  def set_api_key
+    self.api_key = generate_api_key
+  end
+
+  def generate_api_key
+    SecureRandom.base58(28)
+  end
 end
