69def125e2e619149dc5ecdf35ecde84c5c04e57
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 14:10:02 -0700

    Add .new to UsersSerializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: UsersSerializer(user), status: 201
+      render json: UsersSerializer.new(user), status: 201
     else
       render json: { error: user.errors.full_messages.to_sentence }, status: 401
     end
