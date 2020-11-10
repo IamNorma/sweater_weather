31b874247f86af80f957e94a807505e9fe862d6c
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 16:24:19 -0700

    Update status code for sad path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     if user.save
       render json: UsersSerializer.new(user), status: 201
     else
-      render json: { error: user.errors.full_messages.to_sentence }, status: 401
+      render json: { error: user.errors.full_messages.to_sentence }, status: 400
     end
   end
 
