b7dc085c8bf2df03698ec0a0dd25b859619c6cfc
Norma Lopez <norma_l304@yahoo.com>
2020-11-11 00:13:15 -0700

    Add roadtrip poro with attr readers

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,3 +1,15 @@
 class RoadTrip
-
+  attr_reader :start_city,
+              :end_city,
+              :travel_time,
+              :temperature,
+              :conditions
+              
+  def initialize(roadtrip_params, forecast)
+    @start_city = find_start_city(roadtrip_params[:route][:locations])
+    @end_city = find_end_city(roadtrip_params[:route][:locations])
+    @travel_time = format_time(roadtrip_params[:route][:realTime])
+    @temperature = find_temp(forecast)
+    @conditions = find_conditions(forecast)
+  end
 end
