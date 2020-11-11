4af362abb79aac76833ab08fcc57658ddcaf2564
Norma Lopez <norma_l304@yahoo.com>
2020-11-11 02:13:34 -0700

    Add spec for impossible roadtrip route

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -108,7 +108,44 @@ describe "Road Trip API" do
     expect(json[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
     expect(json[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
 
-    expect(json[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Numeric).or(eq(nil))
-    expect(json[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String).or(eq(nil))
+    expect(json[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Numeric)
+    expect(json[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+  end
+
+  it "returns impossible route for New York, NY to London,UK" do
+    params = {
+      "origin": "NewYork,NY",
+      "destination": "London,UK",
+      "api_key": @user.api_key
+    }
+
+    post '/api/v1/road_trip', headers: @headers, params: JSON.generate(params)
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data]).to be_a(Hash)
+
+    expect(json[:data][:id]).to eq(nil)
+    expect(json[:data][:type]).to eq('roadtrip')
+    expect(json[:data]).to have_key(:attributes)
+
+    expect(json[:data][:attributes]).to be_a(Hash)
+
+    expect(json[:data][:attributes]).to have_key(:start_city)
+    expect(json[:data][:attributes][:start_city]).to be_a(String)
+    expect(json[:data][:attributes]).to have_key(:end_city)
+    expect(json[:data][:attributes][:end_city]).to be_a(String)
+    expect(json[:data][:attributes]).to have_key(:travel_time)
+    expect(json[:data][:attributes][:travel_time]).to eq('impossible route')
+    expect(json[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(json[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+
+    expect(json[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(json[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+
+    expect(json[:data][:attributes][:weather_at_eta][:temperature]).to eq(nil)
+    expect(json[:data][:attributes][:weather_at_eta][:conditions]).to eq(nil)
   end
 end
