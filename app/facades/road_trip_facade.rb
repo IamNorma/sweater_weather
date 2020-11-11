cb99cbfd8d7c99e9d903d13328c073425616eb93
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 22:43:30 -0700

    Add latitude and longitude variables and call to WeatherService.weather_forecast() for forecast

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,6 +1,9 @@
 class RoadTripFacade
   def self.fetch_roadtrip_info(origin, destination)
     roadtrip_data = MapService.route(origin, destination)
+    latitude = roadtrip_data[:route][:locations][1][:latLng][:lat]
+    longitude = roadtrip_data[:route][:locations][1][:latLng][:lng]
+    forecast = WeatherService.weather_forecast(latitude, longitude)
     RoadTrip.new(roadtrip_data, forecast)
   end
 end
