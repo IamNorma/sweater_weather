60b2aeeaf7981ed8bff6faf4226687d49bbaca38
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 17:55:42 -0700

    Add route for road trip endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get '/backgrounds', to: 'backgrounds#index'
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
+      post '/road_trip', to: 'roadtrip#index'
     end
   end
 end
