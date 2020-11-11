b76ff5cd456c55f0019da6ebf16e62259bc80b3d
Norma Lopez <norma_l304@yahoo.com>
2020-11-11 00:16:58 -0700

    Add #find_start_city

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -4,12 +4,16 @@ class RoadTrip
               :travel_time,
               :temperature,
               :conditions
-              
+
   def initialize(roadtrip_params, forecast)
     @start_city = find_start_city(roadtrip_params[:route][:locations])
     @end_city = find_end_city(roadtrip_params[:route][:locations])
     @travel_time = format_time(roadtrip_params[:route][:realTime])
     @temperature = find_temp(forecast)
     @conditions = find_conditions(forecast)
+  end
+
+  def find_start_city(data)
+    data[0][:adminArea5] + ', ' + data[0][:adminArea3]
   end
 end
