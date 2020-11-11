f7a3ee6644a6421c29e57727dd2b084fb3d72c41
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 18:45:28 -0700

    Update index action to include sad path

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::RoadTripController < ApplicationController
     if user.present?
       roadtrip = RoadTripFacade.fetch_roadtrip_info(origin, destination)
       render json: RoadTripSerializer.new(roadtrip)
+    elsif params[:api_key].nil? || user.nil?
+      render json: { error: 'Valid API key required' }, status: 401
     end
   end
 end
