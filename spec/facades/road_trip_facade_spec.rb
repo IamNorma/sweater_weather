c12addc56c367d16e4607bb52e81bfcd68715d90
Norma Lopez <norma_l304@yahoo.com>
2020-11-11 01:49:47 -0700

    Change Roadtrip to RoadTrip

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe 'Road Trip Facade' do
 
     roadtrip = RoadTripFacade.fetch_roadtrip_info(origin, destination)
 
-    expect(roadtrip).to be_a(Roadtrip)
+    expect(roadtrip).to be_a(RoadTrip)
 
     expect(roadtrip.start_city).to be_a(String)
-    expect(roadtrip.end_city).to be_an(Integer)
-    expect(roadtrip.travel_time).to be_an(Integer)
+    expect(roadtrip.end_city).to be_an(String)
+    expect(roadtrip.travel_time).to be_an(String)
     expect(roadtrip.temperature).to be_a(Numeric).or(eq(nil))
     expect(roadtrip.conditions).to be_a(String).or(eq(nil))
   end
