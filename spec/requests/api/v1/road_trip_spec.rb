c49bbc962e77862abda17a08ee6fdb116c2210fe
Norma Lopez <norma_l304@yahoo.com>
2020-11-11 02:10:19 -0700

    Add test for roadtrip from LA to NY

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -74,4 +74,41 @@ describe "Road Trip API" do
     expect(response.status).to eq(401)
     expect(json[:error]).to eq("Valid API key required")
   end
+
+  it "returns road trip info for New York, NY to Los Angeles, CA" do
+    params = {
+      "origin": "NewYork,NY",
+      "destination": "LosAngeles,CA",
+      "api_key": @user.api_key
+    }
+
+    post '/api/v1/road_trip', headers: @headers, params: JSON.generate(params)
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data]).to be_a(Hash)
+
+    expect(json[:data][:id]).to eq(nil)
+    expect(json[:data][:type]).to eq('roadtrip')
+    expect(json[:data]).to have_key(:attributes)
+
+    expect(json[:data][:attributes]).to be_a(Hash)
+
+    expect(json[:data][:attributes]).to have_key(:start_city)
+    expect(json[:data][:attributes][:start_city]).to be_a(String)
+    expect(json[:data][:attributes]).to have_key(:end_city)
+    expect(json[:data][:attributes][:end_city]).to be_a(String)
+    expect(json[:data][:attributes]).to have_key(:travel_time)
+    expect(json[:data][:attributes][:travel_time]).to be_an(String)
+    expect(json[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(json[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+
+    expect(json[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(json[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+
+    expect(json[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Numeric).or(eq(nil))
+    expect(json[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String).or(eq(nil))
+  end
 end
