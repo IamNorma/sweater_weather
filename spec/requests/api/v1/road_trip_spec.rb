4ec1f90e8558bbf342d2880fa0e294cd5f605280
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 18:44:06 -0700

    Update spec with before(:each) to have User and headers

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 describe "Road Trip API" do
-  it "returns road trip info " do
-    headers = {
+  before(:each) do
+    @user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
+    @headers = {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     }
+  end
 
+  it "returns road trip info " do
     params = {
       "origin": "Denver,CO",
       "destination": "Pueblo,CO",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
+      "api_key": @user.api_key
     }
 
-    post '/api/v1/road_trip', headers: headers, params: JSON.generate(params)
+    post '/api/v1/road_trip', headers: @headers, params: JSON.generate(params)
 
     expect(response).to be_successful
 
