b2145c71f75ac1f81f2068d96e79d956a08866a4
Norma Lopez <norma_l304@yahoo.com>
2020-11-11 03:16:16 -0700

    Update spec to have expectation of what data should not be included

--- spec/requests/api/v1/city_weather_spec.rb
+++ spec/requests/api/v1/city_weather_spec.rb
@@ -73,4 +73,52 @@ describe "User" do
     expect(forecast[:data][:attributes][:hourly_weather][0]).to have_key(:icon)
     expect(forecast[:data][:attributes][:hourly_weather][0][:icon]).to be_a(String)
   end
+
+  it "can retrieve weather for a city and should not include uneccessary data" do
+
+    get '/api/v1/forecast?location=denver,co'
+
+    expect(response).to be_successful
+
+    forecast = JSON.parse(response.body, symbolize_names: true)
+
+    expect(forecast[:data]).to be_a(Hash)
+
+    expect(forecast[:data][:type]).to eq('forecast')
+    expect(forecast[:data]).to have_key(:attributes)
+
+    expect(forecast[:data][:attributes]).to be_a(Hash)
+
+    expect(forecast[:data][:attributes][:current_weather]).to_not have_key(:pressure)
+    expect(forecast[:data][:attributes][:current_weather]).to_not have_key(:dew_point)
+    expect(forecast[:data][:attributes][:current_weather]).to_not have_key(:clouds)
+    expect(forecast[:data][:attributes][:current_weather]).to_not have_key(:wind_speed)
+    expect(forecast[:data][:attributes][:current_weather]).to_not have_key(:wind_deg)
+    expect(forecast[:data][:attributes][:current_weather]).to_not have_key(:weather)
+    
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:day)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:night)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:eve)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:morn)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:feels_like)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:pressure)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:humidity)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:dew_point)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:wind_speed)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:wind_deg)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:id)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:main)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:clouds)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:pop)
+    expect(forecast[:data][:attributes][:daily_weather][0]).to_not have_key(:uvi)
+
+    expect(forecast[:data][:attributes][:hourly_weather][0]).to_not have_key(:pressure)
+    expect(forecast[:data][:attributes][:hourly_weather][0]).to_not have_key(:dew_point)
+    expect(forecast[:data][:attributes][:hourly_weather][0]).to_not have_key(:clouds)
+    expect(forecast[:data][:attributes][:hourly_weather][0]).to_not have_key(:visibility)
+    expect(forecast[:data][:attributes][:hourly_weather][0]).to_not have_key(:feels_like)
+    expect(forecast[:data][:attributes][:hourly_weather][0]).to_not have_key(:pop)
+    expect(forecast[:data][:attributes][:hourly_weather][0]).to_not have_key(:id)
+    expect(forecast[:data][:attributes][:hourly_weather][0]).to_not have_key(:main)
+  end
 end
