d5ea0efdfc9aa485ffba87eb9b8809fdf84c30cc
Norma Lopez <norma_l304@yahoo.com>
2020-11-10 20:32:42 -0700

    Update ::fetch_roadtrip_info() with appropriate params for origin and destination

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RoadTripController < ApplicationController
   def index
     user = User.find_by(api_key: params[:api_key])
     if user.present?
-      roadtrip = RoadTripFacade.fetch_roadtrip_info(origin, destination)
+      roadtrip = RoadTripFacade.fetch_roadtrip_info(params[:origin], params[:destination])
       render json: RoadTripSerializer.new(roadtrip)
     elsif params[:api_key].nil? || user.nil?
       render json: { error: 'Valid API key required' }, status: 401
